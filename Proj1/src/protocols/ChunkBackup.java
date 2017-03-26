27c424f219ddc71ae5cdb0dca49108f4a5fe985f
teresa-Guilherme <francisco_c_gomes@hotmail.com>
2017-03-26 03:20:45 +0100

    Update ChunkBackup.java

--- Proj1/src/protocols/ChunkBackup.java
+++ Proj1/src/protocols/ChunkBackup.java
@@ -9,9 +9,6 @@ import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import fileManager.Chunk;
 import peer.Peer;
@@ -20,13 +17,14 @@ public class ChunkBackup implements Runnable{
 	
 	private File file;
 	private int replication;
-	
-	private HashMap<String,Integer> backedUp = new HashMap<String,Integer>();
-	private List <String> myChunks = new ArrayList<String>();
+	private int waitingTime;
+	private int numTries;
 	
 	public ChunkBackup(File f, int repl){		
 		file=f;
 		replication=repl;
+		waitingTime=1000;
+		numTries=0;
 	}
 
 	@Override
@@ -37,26 +35,33 @@ public class ChunkBackup implements Runnable{
 		try {
 						
 			BufferedInputStream bufferInput = new BufferedInputStream(new FileInputStream(file));
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update((file.getName()+Long.toString(file.lastModified())).getBytes());
+			String fileID = new String(messageDigest.digest());
+			Peer.addToTranslations(file.getName(), fileID);
 			
 			@SuppressWarnings("unused")
 			int bytesRead;
 			
-			//to implement: Partition of file content into chunks 
-			
 			 while ((bytesRead = bufferInput.read(buffer)) > 0) {
 	             
-				 MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-				 messageDigest.update((file.getName()+Long.toString(file.lastModified())).getBytes());
-				 String fileIDbin = new String(messageDigest.digest());
-				 String fileID = String.format("%040x", new BigInteger(1, fileIDbin.getBytes()));
 				 
-	             Chunk chunk = new Chunk(fileID,chunkNo,buffer,replication);
-	             
+				 	
+	             Chunk chunk = new Chunk(String.format("%040x", new BigInteger(1, fileID.getBytes())),chunkNo,buffer,replication);
+	             Peer.addBackup(chunk,null);
 	             Message message = new Message(chunk);
-	    
-	             sendToMDB(message.createPutChunk());
+				 while(numTries<5 && Peer.getNumberOfPeers(chunk)<chunk.getReplication()){
+					 sendToMDB(message.createPutChunk());
+					 try {
+						Thread.sleep(waitingTime);
+						numTries++;
+						waitingTime*=2;
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 }
 	             
-	             addMyChunks(fileID, chunkNo);
 	             
 	            }
 			 
@@ -79,16 +84,5 @@ public class ChunkBackup implements Runnable{
 	public int getReplication() {
 		return replication;
 	}
-	
-	public void addMyChunks(String id, int chunkNo){
-		myChunks.add(id+ " "+ Integer.toString(chunkNo));
-	}
-	
-	public boolean isMyChunk(String id, int chunkNo){
-		return myChunks.contains(id+ " "+ Integer.toString(chunkNo));		
-	}
 
-	public void addToBackedUp(int i,String str){
-		backedUp.put(str, i);
-	}
 }
